4b13519b90addcd97ed5a7c4448b41284b0fa022
David Thomas Loris <davidloris@gmail.com>
2014-04-27 11:28:35 +0200

    assigment 3
    completed functions for peer assessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions are used for creating, a matrix, then calculating, caching 
+## and retrieving its inverse
 
-## Write a short comment describing this function
+## This function creates and caches matrix and allows accessing 
+## of the matrix and its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  } 
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse )
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## this function caches the inverse of matrix so that it can be retrieved by
+## the previous function
+
+cachesolve <- function(x,...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
 }
